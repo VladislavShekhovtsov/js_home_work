be7741a2b68a36b8d9a1b8b44e5b628e183bfed4
VladShekhovtsov <vlad.shekhovtsov93@gmail.com>
2024-01-03 13:39:53 +0200

    new home work

--- 08_if &switch/main.js
+++ 08_if &switch/main.js
@@ -169,3 +169,97 @@
         })
 
 }
+
+{   // closure calc 2
+
+    fetch('https://open.er-api.com/v6/latest/USD').then(res => res.json())
+        .then(data => {
+
+            const selectFrom = document.getElementById('from');
+            const selectTo = document.getElementById('to');
+            const divRate = document.getElementById('rate');
+            const inputAmount = document.getElementById('amount');
+            const divResult = document.getElementById('result');
+            selectFrom.style.width = '100px';
+            selectFrom.style.textAlign = 'center';
+            selectTo.style.width = '100px';
+            selectTo.style.textAlign = 'center';
+
+
+            for (const currency in data.rates) {
+                const optionFrom = document.createElement('option');
+                optionFrom.innerText += currency;
+                selectFrom.append(optionFrom);
+
+                const optionTo = document.createElement('option');
+                optionTo.innerText += currency
+                selectTo.append(optionTo);
+
+            }
+
+            const updateRate = () => {
+                const fromValue = from.value;
+                const toValue = to.value;
+
+                const crossRate = data.rates[toValue] / data.rates[fromValue]
+                divRate.innerText = crossRate.toFixed(4);
+
+                updateResult();
+            }
+
+            selectFrom.onchange = updateRate;
+            selectTo.onchange = updateRate;
+
+            const updateResult = () => {
+                const fromValue = from.value;
+                const toValue = to.value;
+                const amount = +inputAmount.value;
+
+                const resultAmount = amount * (data.rates[toValue] / data.rates[fromValue])
+
+                divResult.innerText = resultAmount.toFixed(4);
+            }
+
+            inputAmount.oninput = updateResult;
+
+            updateRate();
+
+            console.log(data)
+        })
+
+}
+
+{   // countries and cities
+
+    fetch('https://raw.githubusercontent.com/russ666/all-countries-and-cities-json/master/countries.min.json').then(res => res.json())
+        .then(data => {
+
+            const countriesSelected = document.getElementById('countries');
+            const citiesSelected = document.getElementById('cities');
+
+            for (const countries in data) {
+                const optionCountries = document.createElement('option');
+                optionCountries.value = countries;
+                optionCountries.innerText += countries;
+                countriesSelected.append(optionCountries);
+
+            }
+
+            const functionOnChange = () => {
+                const citiesOfCountry = countriesSelected.value;
+                    citiesSelected.innerHTML = ''
+                for (const city of data[citiesOfCountry]) {
+                    const optionSities = document.createElement('option');
+                    optionSities.value = city;
+                    optionSities.innerText += city
+                    citiesSelected.append(optionSities);
+                }
+            }
+
+            countriesSelected.onchange = functionOnChange;
+
+
+            console.log(data)
+        })
+
+}
